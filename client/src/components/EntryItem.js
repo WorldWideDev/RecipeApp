bf8ee71f397d75b2563de54ae5201e186cba41b1
devon <dev206@gmail.com>
2021-08-14 14:23:41 -0700

    entry item conditional 'edit' vs 'cancel'

--- client/src/components/EntryItem.js
+++ client/src/components/EntryItem.js
@@ -4,6 +4,7 @@ const EntryItem = (props) => {
     const { value, onEntryItemChanged, onEntryItemDeleted } = props;
     const [isEdit, setIsEdit] = useState(false);
     const [text, setText] = useState(value);
+    const [hasChanged, setHasChanged] = useState(false);
 
     function renderDefault() {
         return <span onClick={() => setIsEdit(true)}>{value}</span>
@@ -11,16 +12,21 @@ const EntryItem = (props) => {
     function onUpdateHandler() {
         setIsEdit(false);
         onEntryItemChanged(text);
+        setHasChanged(false);
+    }
+    function onChangeHandler(e) {
+        setText(e.target.value);
+        setHasChanged(true);
     }
     function renderEditing() {
         return (
             <div className="entry-item-form-group">
                 <input autoFocus className="form-control"
                     type="text" value={text} 
-                    onChange={(e) => setText(e.target.value)} 
+                    onChange={(e) => onChangeHandler(e)} 
                 />
                 <button className="btn btn-warning"
-                    onClick={onUpdateHandler}>Edit</button>
+                    onClick={onUpdateHandler}>{ hasChanged ? "Edit" : "Cancel" }</button>
                 <button className="btn btn-danger"
                     onClick={onEntryItemDeleted}>Delete</button>
             </div>
