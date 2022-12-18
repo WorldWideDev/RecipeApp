69b5068236e59bb43bbf6b7526211e298b90bc3b
dev <dev206@gmail.com>
2022-12-18 14:58:26 -0800

    removing server app, keeping seed data

--- server/config/mongoose.config.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/recipes", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log("Established a connection to the db"))
-    .catch(err => console.log("Something went wrong when connecting to the db", err))
--- server/routes/recipe.routes.js
+++
@@ -1,8 +0,0 @@
-const RecipeController = require('../controllers/recipe.controller.js');
-module.exports = function(app) {
-    app.get('/api', RecipeController.getAll);
-    app.post('/api', RecipeController.create);
-    app.get('/api/:id', RecipeController.getOne);
-    app.put('/api/:id', RecipeController.update);
-    app.delete('/api/:id', RecipeController.delete);
-}
